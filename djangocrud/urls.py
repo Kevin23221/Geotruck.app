4f3703765b7328d2865e362d1e17e81d796923f8
jkest07 <ejuan5427@gmail.com>
2026-06-12 16:38:48 -0500

    Dashboard

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('tareas/', views.rutas, name='rutas'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
 
