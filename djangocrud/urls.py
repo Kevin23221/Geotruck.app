9492a9e521efaf1a1716f80ce86d8f3bc4ef133c
kevin23221 <guacanemekevin2007@gmail.com>
2026-06-19 15:30:54 -0500

    creacion-base

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('tareas/', views.rutas, name='rutas'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('rutas/', views.rutas, name='rutas'),                                       
+    path('dashboard/rutas/', views.rutas, name='rutas'),                                       
     path('rutas/agregar/', views.agregar_ruta, name='agregar_ruta'),                  
     path('rutas/eliminar/<int:ruta_id>/', views.eliminar_ruta, name='eliminar_ruta'),
-    path('vehiculos/', views.vehiculos, name='vehiculos'),
+    path('dashboard/vehiculos/', views.vehiculos, name='vehiculos'),
     path('vehiculos/agregar/', views.agregar_vehiculo, name='agregar_vehiculo'),
     path('vehiculos/eliminar/<int:vehiculo_id>/', views.eliminar_vehiculo, name='eliminar_vehiculo'),
 ]
